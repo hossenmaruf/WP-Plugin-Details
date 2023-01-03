6e69a67b2a1077c77d88db58024a369e6d92cf3b
hossenmaruf <severusmaruf740@gmail.com>
2023-01-04 02:50:47 +0600

    Form Submit & send by email

--- new-plugin.php
+++ new-plugin.php
@@ -100,3 +100,116 @@ function my_custom_menu_page()
 
 
 }
+
+
+function display_header_scripts()
+{
+
+    $header_scripts = get_option('new_header_scripts', 'none');
+
+    print $header_scripts;
+}
+
+add_action('wp_head', 'display_header_scripts');
+
+
+function display_footer_scripts()
+{
+
+    $footer_scripts = get_option('new_footer_scripts', 'none');
+
+    print $footer_scripts;
+}
+
+add_action('wp_footer', 'display_footer_scripts');
+
+
+
+
+function ideapro_form()
+{
+    /* content variable */
+    $content = '';
+
+    $content .= '<form method="post" action="http://new.local/thank-you/">';
+
+        $content .= '<input type="text" name="full_name" placeholder="Your Full Name" />';
+        $content .= '<br />';
+
+        $content .= '<input type="text" name="email_address" placeholder="Email Address" />';
+        $content .= '<br />';
+
+        $content .= '<input type="text" name="phone_number" placeholder="Phone Number" />';
+        $content .= '<br />';
+
+        $content .= '<textarea name="comments" placeholder="Give us your comments"></textarea>';
+        $content .= '<br />';
+
+        $content .= '<input type="submit" name="ideapro_submit_form" value="SUBMIT YOUR INFORMATION" />';
+
+    $content .= '</form>';
+
+    return $content;
+}
+add_shortcode('ideapro_contact_form','ideapro_form');
+
+
+function set_html_content_type()
+{
+    return 'text/html';
+
+}
+
+
+
+function ideapro_form_capture()
+{
+    global $post,$wpdb;
+
+    if(array_key_exists('ideapro_submit_form',$_POST))
+    {
+        $to = "fromhome380@gmail.com";
+        $subject = "Idea Pro Example Site Form Submission";
+        $body = '';
+
+        $body .= 'Name: '.$_POST['full_name'].' <br /> ';
+        $body .= 'Email: '.$_POST['email_address'].' <br /> ';
+        $body .= 'Phone: '.$_POST['phone_number']. ' <br /> ';
+        $body .= 'Comments: '.$_POST['comments'].' <br /> ';
+
+
+        add_filter('wp_mail_content_type','set_html_content_type');
+        
+        wp_mail($to,$subject,$body);
+
+        remove_filter('wp_mail_content_type','set_html_content_type');
+
+        /* Insert the information into a comment */
+
+        /*$time = current_time('mysql');
+
+        $data = array(
+            'comment_post_ID' => $post->ID,
+            'comment_content' => $body,
+            'comment_author_IP' => $_SERVER['REMOTE_ADDR'],
+            'comment_date' => $time,
+            'comment_approved' => 1,
+        );
+
+        wp_insert_comment($data);*/
+
+        /* add the submission to the database using the table we created */ 
+        $insertData = $wpdb->get_results(" INSERT INTO ".$wpdb->prefix."form_submissions (data) VALUES ('".$body."') ");
+
+
+
+    }
+
+}
+add_action('wp_head','ideapro_form_capture');
+
+
+
+
+
+
